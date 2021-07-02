be95d068ff0503a01d4b2ba951683892944ea1df
omipapal12 <omipapal12@gmail.com>
2021-07-02 16:12:45 +0530

    Add PartTime EmpWage

--- empWage.sh
+++ empWage.sh
@@ -1,9 +1,13 @@
 #!/bin/bash -x
-empCheck=$((RANDOM%2))
+empCheck=$((RANDOM%3))
+empRatePerHr=20
+
 if [ $empCheck -eq 1 ];then
-	empRatePerHr=20
 	empHrs=8
-	wage=$(($empRatePerHr*$empHrs))
+elif [ $empCheck -eq 2 ];then
+	empHrs=4
 else
-	wage=0
+	empHrs=0
 fi
+salary=$(($empRatePerHr*$empHrs))
+echo $salary
