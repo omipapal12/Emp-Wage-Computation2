60af960c702c5d5f4efb50b86ba34dafdcb22c07
omipapal12 <omipapal12@gmail.com>
2021-07-02 16:52:28 +0530

    EmpWage of total working hour or days

--- empWage.sh
+++ empWage.sh
@@ -1,20 +1,28 @@
 #!/bin/bash -x
+#costants
+IS_FULLTIME=1
+IS_PARTTIME=2
 empRatePerHr=20
 numOfworkingDays=20
-totalSalary=0
-for ((day=1;day<=$numOfworkingDays;day++))
+MAX_HRS_IN_MONTH=100
+
+#Variables
+totalEmpHrs=0
+totalWorkingDays=0
+
+while [[ $totalEmpHrs -lt $MAX_HRS_IN_MONTH && $totalWorkingDays -lt $numOfworkingDays ]]
 do
+	((totalWorkingDays++))
 	empCheck=$((RANDOM%3))
 	case $empCheck in
-		1) empHrs=8
+		$IS_FULLTIME) empHrs=8
 		;;
-		2) empHrs=4
+		$IS_PARTTIME) empHrs=4
 		;;
 		*) empHrs=0
 		;;
 	esac
-	salary=$(($empRatePerHr*$empHrs))
-	totalSalary=$(($totalSalary+$salary))
+	totalEmpHrs=$(($totalEmpHrs+$empHrs))
 done
-echo $totalSalary
-© 2021 GitHub, Inc.
+Wage=$(($totalEmpHrs*$empRatePerHr))
+echo $Wage
