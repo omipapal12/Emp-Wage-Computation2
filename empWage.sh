05997c5fbef8890d5bc6654c6f072e15ff4c08d8
omipapal12 <omipapal12@gmail.com>
2021-07-02 16:58:27 +0530

    Refactoring the code to get work hours

--- empWage.sh
+++ empWage.sh
@@ -10,19 +10,27 @@ MAX_HRS_IN_MONTH=100
 totalEmpHrs=0
 totalWorkingDays=0
 
+getWorkingHours(){
+	case $1 in
+      $IS_FULLTIME) empHrs=8
+      ;;
+      $IS_PARTTIME) empHrs=4
+      ;;
+      *) empHrs=0
+      ;;
+   esac
+	echo $empHrs
+
+}
+
 while [[ $totalEmpHrs -lt $MAX_HRS_IN_MONTH && $totalWorkingDays -lt $numOfworkingDays ]]
 do
 	((totalWorkingDays++))
 	empCheck=$((RANDOM%3))
-	case $empCheck in
-		$IS_FULLTIME) empHrs=8
-		;;
-		$IS_PARTTIME) empHrs=4
-		;;
-		*) empHrs=0
-		;;
-	esac
-	totalEmpHrs=$(($totalEmpHrs+$empHrs))
+	workHours=$( getWorkingHours $empCheck )
+	totalEmpHrs=$(($totalEmpHrs+$workHours))
+
 done
 Wage=$(($totalEmpHrs*$empRatePerHr))
 echo $Wage
+
