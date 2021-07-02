5c4859991a331e99f3110b94718300800d0d4161
omipapal12 <omipapal12@gmail.com>
2021-07-02 16:37:11 +0530

    EmpWage for a month

--- empWage.sh
+++ empWage.sh
@@ -1,13 +1,20 @@
 #!/bin/bash -x
-empCheck=$((RANDOM%3))
 empRatePerHr=20
-
-if [ $empCheck -eq 1 ];then
-	empHrs=8
-elif [ $empCheck -eq 2 ];then
-	empHrs=4
-else
-	empHrs=0
-fi
-salary=$(($empRatePerHr*$empHrs))
-echo $salary
+numOfworkingDays=20
+totalSalary=0
+for ((day=1;day<=$numOfworkingDays;day++))
+do
+	empCheck=$((RANDOM%3))
+	case $empCheck in
+		1) empHrs=8
+		;;
+		2) empHrs=4
+		;;
+		*) empHrs=0
+		;;
+	esac
+	salary=$(($empRatePerHr*$empHrs))
+	totalSalary=$(($totalSalary+$salary))
+done
+echo $totalSalary
+© 2021 GitHub, Inc.
